1b7f66ebe9bc5a593167047ffdb651ffde69611b
Authachai Murnpimai <authachai.m@clicknext.com>
2023-10-19 16:30:21 +0700

    UPDATE Vuetify

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,16 @@
 export default defineNuxtConfig({
     modules: [
         '@nuxt/devtools',
+        ['@nuxtjs/google-fonts', {
+            families: {
+                'Noto Sans Thai': true,
+                download: true,
+                inject: true
+            }
+        }]
     ],
-    css: ['~/assets/styles/main.scss'],
+    css: ['~/assets/styles/main.scss', 'vuetify/lib/styles/main.sass'],
+    build: {
+        transpile: ['vuetify'],
+    }
 })
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,11 @@
 <template>
-    <div>
-        <button class="btn">GGG</button>
-    </div>
+    <v-container class="grey lighten-5">
+        <v-row no-gutters>
+            <v-col style="background-color: rgb(240, 240, 240); border-radius: 8px;">
+                <v-row style="padding:32px;">
+                    <h3 class="primary">payment</h3>
+                </v-row>
+            </v-col>
+        </v-row>
+    </v-container>
 </template>
