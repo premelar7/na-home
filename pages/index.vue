4023f555bb3ba34e6f7cd5d04ae92f244768c542
Authachai Murnpimai <authachai.m@clicknext.com>
2024-02-06 16:44:27 +0700

    V3

--- pages/index.vue
+++ pages/index.vue
@@ -1,68 +1,69 @@
 <template>
     <v-container>
         <v-row style="padding:30px 8px 8px 8px;">
-            <v-col style="border-radius: 10px; box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19); ">
-                <div style="padding: 30px 20px 40px 20px;">
+            <v-col
+                style="border-radius: 25px; box-shadow: 0 3px 5px 0 rgba(150, 150, 150, 0.2), 0 6px 20px 0 rgba(124, 124, 124, 0.19); ">
+                <div style="padding: 30px 7% 40px 7%">
+
                     <v-row>
                         <v-col cols="12">
-                            <h2 class="primary">NA Home payment</h2>
+                            <h2 class="primary">Payment</h2>
                         </v-col>
                     </v-row>
 
                     <v-row>
-                        <v-col cols="12">
+                        <v-col cols="12" style="padding-bottom: 6px;">
                             <p>รายการใช้จ่าย</p>
                         </v-col>
                         <v-col cols="12" class="row-content">
-                            <v-text-field label="ตั้งชื่อรายการก่อน" outlined></v-text-field>
+                            <v-text-field label="รายการใช้จ่าย" outlined></v-text-field>
                         </v-col>
                     </v-row>
 
                     <v-row style="margin-top: 0px;">
-                        <v-col cols="12">
+                        <v-col cols="12" style="padding-bottom: 6px;">
                             <p>ราคา (฿)</p>
                         </v-col>
-                        <v-col cols="12" md="6" xl="3" class="row-content">
-                            <v-text-field label="เท่าไหร่นะ" outlined></v-text-field>
+                        <v-col cols="12" class="row-content">
+                            <v-text-field label="จำนวนเงิน" outlined></v-text-field>
                         </v-col>
                     </v-row>
 
                     <v-row style="margin-top: 0px;">
-                        <v-col cols="12">
+                        <v-col cols="12" style="padding-bottom: 6px;">
                             <p>คนจ่าย</p>
                         </v-col>
-                        <v-col cols="12" md="6" xl="3" class="row-content">
-                            <v-select :items="pay_man" outlined placeholder="คุณน้อตหรือคุณอรรถเอ่ย"></v-select>
+                        <v-col cols="12" class="row-content">
+                            <v-select :items="pay_man" outlined placeholder="คนออกเงิน"></v-select>
                         </v-col>
                     </v-row>
 
                     <v-row style="margin-top: 0px;">
-                        <v-col cols="12">
+                        <v-col cols="12" style="padding-bottom: 6px;">
                             <p>หารไหม</p>
                         </v-col>
-                        <v-col cols="12" md="6" xl="3" class="row-content">
-                            <v-select :items="shared_options" outlined
-                                placeholder="หารคือหาร, ไม่หารคือคนจ่ายจะได้คืนตามจำนวนข้างบนนะ"></v-select>
+                        <v-col cols="12" class="row-content">
+                            <v-select :items="shared_options" outlined placeholder="หารหรือไม่"></v-select>
                         </v-col>
                     </v-row>
 
                     <v-row style="margin-top: 0px;">
-                        <v-col cols="12">
-                            <p>แนบ slip ใดๆ</p>
+                        <v-col cols="12" style="padding-bottom: 6px;">
+                            <p>แนบรูปแนบสลิปใดๆ</p>
                         </v-col>
-                        <v-col cols="12" md="6" xl="3" class="row-content">
+                        <v-col cols="12" class="row-content">
                             <v-file-input hide-input>
                             </v-file-input>
                         </v-col>
                     </v-row>
 
-                    <v-row style="padding-top:30px;">
+                    <v-row style="padding-top:20px;">
                         <v-col cols="2"></v-col>
                         <v-col cols="12" sm="4">
                             <v-btn x-large>ยกเลิกการสร้าง</v-btn>
                         </v-col>
                         <v-col cols="12" sm="4">
-                            <v-btn color="primary" x-large>สร้างรายการ</v-btn>
+                            <v-btn color="#000" x-large>สร้างรายการ</v-btn>
                         </v-col>
                         <v-col cols="2"></v-col>
                     </v-row>
