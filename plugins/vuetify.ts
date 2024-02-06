9361c553e77ef8ce9c73c59052915166cec83763
Authachai Murnpimai <authachai.m@clicknext.com>
2024-02-06 16:57:23 +0700

    V55

--- plugins/vuetify.ts
+++ plugins/vuetify.ts
@@ -5,29 +5,13 @@ import { aliases, custom } from "../utils/customIcons"
 
 export default defineNuxtPlugin((app) => {
   const vuetify = createVuetify({
-    // ssr: true,
     defaults,
-    // add theme
     theme: {
       themes: {
         light,
         dark
-      },
-      // add color variations
-      //   variations: {
-      //     colors: ["primary", "secondary"],
-      //     lighten: 3,
-      //     darken: 3,
-      //   },
-    },
-    // Add the custom iconset
-    icons: {
-      defaultSet: "custom",
-      aliases,
-      sets: {
-        custom,
-      },
-    },
+      }
+    }
   });
 
   app.vueApp.use(vuetify);
